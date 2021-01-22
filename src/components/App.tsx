3b1d36929ec49a7328c5fe0bd0cd3dc2e016b4c2
pfdzm <pfdzm@users.noreply.github.com>
2021-01-22 19:49:29 +0100

    Put the form behind a private route

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import { Toaster } from 'react-hot-toast';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import { PrivateRoute, ProvideAuth } from '../hooks/useAuth';
 import Card from './Card';
+import Form from './Form';
 import Layout from './Layout';
 import LoginPage from './LoginPage';
 import TextWithBtn from './TextWithBtn';
@@ -20,6 +21,9 @@ const App: React.FC = () => {
             <li>
               <Link to="/private">Private Area</Link>
             </li>
+            <li>
+              <Link to="/form">Form</Link>
+            </li>
           </ul>
         </nav>
         <Toaster />
@@ -29,6 +33,7 @@ const App: React.FC = () => {
               <Route path="/auth" children={<TextWithBtn />} />
               <Route path="/login" children={<LoginPage />} />
               <PrivateRoute path="/private" children={<TextWithBtn />} />
+              <PrivateRoute path="/form" children={<Form />} />
               <Route path="/" children={<TextWithBtn />} />
             </Switch>
           </Card>
